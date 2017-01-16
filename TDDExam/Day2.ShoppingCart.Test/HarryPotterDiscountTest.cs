4d00b2b305ca6865640550f8b88a5006fc29f9ec
Hans Chen <hans0228@outlook.com>
2017-01-16 22:06:44 +0800

    加入測試案例 06：一二集各買了一本，第三集買了兩本，價格應為100*3*0.9 + 100 = 370

--- TDDExam/Day2.ShoppingCart.Test/HarryPotterDiscountTest.cs
+++ TDDExam/Day2.ShoppingCart.Test/HarryPotterDiscountTest.cs
@@ -90,6 +90,7 @@ namespace Day2.ShoppingCart.Test
             Assert.AreEqual(expected, actual);
         }
 
+
         [TestMethod]
         public void Buy_First_To_Fifth_Each_One_Book()
         {
@@ -111,5 +112,28 @@ namespace Day2.ShoppingCart.Test
             // assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Buy_First_To_Second_Each_One_And_Buy_Third_Two_Book()
+        {
+            // arrange
+            var target = new HarryPotterDiscount();
+            var shoppingItems = new SaleBook[] {
+                new SaleBook { ISBN="9573317249", Name="哈利波特(1)：神秘的魔法石", Price=100, Count=1 },
+                new SaleBook { ISBN="9573317583", Name="哈利波特(2)：消失的密室", Price=100, Count=1 },
+                new SaleBook { ISBN="9573318008", Name="哈利波特(3)：阿茲卡班的逃犯", Price=100, Count=2 },
+                new SaleBook { ISBN="9573318318", Name="哈利波特(4)：火盃的考驗", Price=100, Count=0 },
+                new SaleBook { ISBN="9573319861", Name="哈利波特(5)：鳳凰會的密令", Price=100, Count=0 }
+            };
+
+            var expected = 375m;
+
+            // act
+            var actual = target.CaculateDiscount(shoppingItems);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
