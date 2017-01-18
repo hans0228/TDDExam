4c08bb452d5f5be4c2ae669d7ae6c5ee157cb18c
Hans Chen <hans0228@outlook.com>
2017-01-18 22:08:09 +0800

    簡化書籍種類與折扣的判斷處理

--- TDDExam/Day2.ShoppingCart/HarryPotterDiscount.cs
+++ TDDExam/Day2.ShoppingCart/HarryPotterDiscount.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Day2.ShoppingCart.Test;
 using Day1.GroupSumByColumn;
 using System.Linq;
@@ -7,12 +8,24 @@ namespace Day2.ShoppingCart
 {
     public class HarryPotterDiscount
     {
-        private string[] harryPotters ={
+        // 哈利波特書籍編號
+        private readonly string[] _harryPotters ={
             "9573317249",
             "9573317583",
             "9573318008",
             "9573318318",
             "9573319861"};
+
+        // 書籍種類與折扣 mapping 
+        private readonly Dictionary<int, decimal> _discountByBookTypes =
+            new Dictionary<int, decimal>
+        {
+            {1,1 },
+            {2,0.95m },
+            {3,0.9m },
+            {4,0.8m },
+            {5,0.75m }
+        };
 
         public HarryPotterDiscount()
         {
@@ -21,39 +34,18 @@ namespace Day2.ShoppingCart
         public decimal CaculateDiscount(SaleBook[] shoppingItems)
         {
             // 找出 HarryPotter 系列書籍，產生一個新的集合
-            var books = shoppingItems.Where(x => harryPotters.Contains(x.ISBN) 
+            var books = shoppingItems.Where(x => _harryPotters.Contains(x.ISBN)
                                                  && x.Count > 0).ToArray();
 
             var totalAmount = 0m;
 
-            while (books.SumByColumn(x => x.Count) > 0)
+            while (books.Sum(x => x.Count) > 0)
             {
                 // 書籍種類個數
                 var bookTypes = books.Count();
-                if (bookTypes == 5)
-                {
-                    totalAmount += books.SumByColumn(x => x.Price) * 0.75m;
-                }
 
-                if (bookTypes == 4)
-                {
-                    totalAmount += books.SumByColumn(x => x.Price) * 0.8m;
-                }
+                totalAmount += books.Sum(x => x.Price) * _discountByBookTypes[bookTypes];
 
-                if (bookTypes == 3)
-                {
-                    totalAmount += books.SumByColumn(x => x.Price) * 0.9m;
-                }
-
-                if (bookTypes == 2)
-                {
-                    totalAmount += books.SumByColumn(x => x.Price) * 0.95m;
-                }
-
-                if (bookTypes == 1)
-                {
-                    totalAmount += books.SumByColumn(x => x.Price);
-                }
 
                 // 避免有 multiple enumraton 的問題，補上 ToArray()
                 // 行號 25、64，不加上 ToArray()，測試案例就會有錯
